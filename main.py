3504163ac8a1e313f66ded79005b79de34536d50
saymurrmeow <r.savinovskii@gmail.com>
2023-03-05 04:31:32 +0300

    feat: get active containers info

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import logging
 import sys
 
 from fastapi import FastAPI
+from fastapi.encoders import jsonable_encoder
 import docker
 import uvicorn
 
@@ -11,6 +12,27 @@ AUTH_TOKEN = os.getenv('CI_TOKEN', None)
 
 log = logging.getLogger(__name__)
 app = FastAPI()
+
+
+def get_active_containers():
+    containers = docker_client.containers.list()
+    result = []
+    for container in containers:
+        result.append({
+            'short_id': container.short_id,
+            'container_name': container.name,
+            'image_name': container.image.tags,
+            'created_at': container.attrs['Created'],
+            'status': container.status,
+            'ports': container.ports,
+        })
+
+    return result
+
+
+@app.get('/info')
+async def containers_info():
+    return jsonable_encoder(get_active_containers())
 
 
 def main():
